90ae83c0b7ec5e347f69ee7d90565086688a8a0a
sonish13 <lamsal.sonish@gmail.com>
2024-11-05 11:52:46 -0600

    fix legend names in geom_variety

--- R/geom_variety.R
+++ R/geom_variety.R
@@ -15,9 +15,10 @@
 #'   - subgroup
 #'
 #' @section Computed variables:
-#'
-#'   \describe{ \item{probs}{The probability associated with the highest density region, specified
-#'   by `probs` argument.} }
+#'   \describe{
+#'     \item{probs}{The probability associated with the highest density region, specified
+#'     by `probs` argument.}
+#'   }
 #'
 #' @inheritParams ggplot2::geom_path
 #' @seealso [geom_path()]
@@ -26,58 +27,56 @@
 #' # Basic plots
 #' p <- mpoly::mp("y - x")
 #' ggplot() +
-#'  geom_variety(poly = p, xlim = c(-2, 2), ylim = c(-2, 2))
-
+#'   geom_variety(poly = p, xlim = c(-2, 2), ylim = c(-2, 2))
+#'
 #' p <- mpoly::mp("y - x^2")
 #' ggplot() +
-#'  geom_variety(poly = p, xlim = c(-2, 2), ylim = c(-2, 2))
+#'   geom_variety(poly = p, xlim = c(-2, 2), ylim = c(-2, 2))
+#'
 #' p <- mpoly::mp("x^2 + y^2 - 1")
 #' ggplot() +
-#'  geom_variety(poly = p, xlim = c(-2, 2), ylim = c(-2, 2))
+#'   geom_variety(poly = p, xlim = c(-2, 2), ylim = c(-2, 2))
+#'
 #' p1 <- mp("x^2 + y^2 - 1")
 #' p2 <- mp("y - x")
 #' poly <- mpolyList(p1, p2)
 #' ggplot() +
-#'   geom_variety(poly = poly , xlim = c(-2, 2), ylim = c(-2, 2)) +
+#'   geom_variety(poly = poly, xlim = c(-2, 2), ylim = c(-2, 2)) +
 #'   coord_equal()
-#' (p <- lissajous(7, 7,  0, 0))
-#' ggplot() +
-#'   geom_variety(poly = p, xlim = c(-2, 2), ylim = c(-2, 2), n=201)
 #'
-#' ## setting limits
-##################################################
+#' (p <- lissajous(7, 7, 0, 0))
+#' ggplot() +
+#'   geom_variety(poly = p, xlim = c(-2, 2), ylim = c(-2, 2), n = 201)
+#'
+#' ## Setting limits
 #' p <- mpoly::mp("y - x^2")
 #' ggplot() +
-#'  geom_variety(poly = p) +
-#'  coord_equal()
-#' ggplot() +
-#'  geom_variety(poly = p, xlim = c(-2, 2), ylim = c(-2, 2)) +
-#'  coord_equal()
+#'   geom_variety(poly = p) +
+#'   coord_equal()
 #'
-
-
-
-
+#' ggplot() +
+#'   geom_variety(poly = p, xlim = c(-2, 2), ylim = c(-2, 2)) +
+#'   coord_equal()
 
 #' @rdname geom_variety
 #' @export
 stat_variety <- function(
-  mapping = NULL,
-  data = NULL,
-  geom = GeomVariety,
-  position = "identity",
-  ...,
-  poly = NULL,
-  n = 101,
-  nx = n,
-  ny = n,
-  xlim = NULL,
-  ylim = NULL,
-  na.rm = FALSE,
-  show.legend = NA,
-  inherit.aes = TRUE
+    mapping = NULL,
+    data = NULL,
+    geom = GeomVariety,
+    position = "identity",
+    ...,
+    poly = NULL,
+    n = 101,
+    nx = n,
+    ny = n,
+    xlim = NULL,
+    ylim = NULL,
+    na.rm = FALSE,
+    show.legend = NA,
+    inherit.aes = TRUE
 ) {
-
+  # workaround for data = NULL
   if (is.null(data)) data <- ensure_nonempty_data
 
   layer(
@@ -101,7 +100,6 @@ stat_variety <- function(
   )
 }
 
-
 #' @rdname geom_variety
 #' @format NULL
 #' @usage NULL
@@ -114,38 +112,27 @@ StatVariety <- ggproto(
     self, data, scales, na.rm = FALSE,
     poly, n = 101, nx = n, ny = n, xlim = NULL, ylim = NULL
   ) {
-    if (is.null(xlim)) {
-      if (!is.null(scales$x)) {
-        rangex <- scales$x$dimension()
-      } else {
-        rangex <- c(-1, 1)  # Default value
-      }
-    } else {
-      rangex <- xlim
-    }
+    rangex <- if (is.null(xlim)) {
+      if (!is.null(scales$x)) scales$x$dimension() else c(-1, 1)
+    } else xlim
 
-    if (is.null(ylim)) {
-      if (!is.null(scales$y)) {
-        rangey <- scales$y$dimension()
-      } else {
-        rangey <- c(-1, 1)  # Default value
-      }
-    } else {
-      rangey <- ylim
-    }
-
+    rangey <- if (is.null(ylim)) {
+      if (!is.null(scales$y)) scales$y$dimension() else c(-1, 1)
+    } else ylim
+      # calculation for mpoly
     if (is.mpoly(poly)) {
       dfxyz <- poly_to_df(poly, rangex, rangey, nx, ny)
       isolines <- xyz_to_isolines(dfxyz, 0)
       df <- iso_to_path(isolines, data$group[1])
-      df$variety_id <- as.factor(1)
+      df$Variety <- poly |> mpoly_to_stan() |> as.character()
       return(df)
     } else if (is.mpolyList(poly)) {
+      #calculation for mpolyList
       data_list <- lapply(seq_along(poly), function(i) {
         dfxyz <- poly_to_df(poly[[i]], rangex, rangey, nx, ny)
         isolines <- xyz_to_isolines(dfxyz, 0)
         df <- iso_to_path(isolines, paste0(data$group[1], "_", i))
-        df$variety_id <- as.factor(i)
+        df$Variety <- poly[[i]] |> mpoly_to_stan() |> as.character()
         return(df)
       })
       combined_data <- dplyr::bind_rows(data_list)
@@ -155,7 +142,6 @@ StatVariety <- ggproto(
     }
   }
 )
-
 
 #' @rdname geom_variety
 #' @format NULL
@@ -172,7 +158,6 @@ GeomVariety <- ggproto(
   )
 )
 
-
 #' @rdname geom_variety
 #' @export
 geom_variety <- function(
@@ -186,12 +171,13 @@ geom_variety <- function(
     show.legend = NA,
     inherit.aes = TRUE
 ) {
+  # workaround for data = NULL
   if (is.null(data)) data <- ensure_nonempty_data
 
-  if (is.null(mapping)) {
-    mapping <- aes(group = after_stat(group), colour = after_stat(variety_id))
+  mapping <- if (is.null(mapping)) {
+    aes(group = after_stat(group), colour = after_stat(Variety))
   } else {
-    mapping <- modifyList(mapping, aes(group = after_stat(group), colour = after_stat(variety_id)))
+    modifyList(mapping, aes(group = after_stat(group), colour = after_stat(Variety)))
   }
 
   layer(
@@ -210,11 +196,11 @@ geom_variety <- function(
   )
 }
 
-
-poly_to_df <- function( poly, xlim, ylim, nx, ny ) {
-
+# Function to get the value of polynomial in a grid
+poly_to_df <- function(poly, xlim, ylim, nx, ny) {
   if (!is.mpoly(poly)) poly <- mp(poly)
-  f <- as.function( x = poly, varorder = c("x", "y"), silent = TRUE )
+  f <- as.function(x = poly, varorder = c("x", "y"), silent = TRUE)
+
   df <- expand.grid(
     "x" = seq(xlim[1], xlim[2], length.out = nx),
     "y" = seq(ylim[1], ylim[2], length.out = ny)
@@ -222,4 +208,11 @@ poly_to_df <- function( poly, xlim, ylim, nx, ny ) {
 
   df$z <- with(df, f(cbind(x, y)))
   df
+}
+
+# Used for printing Variety names
+mpoly_to_stan <- function(mpoly) {
+  p <- get("print.mpoly", asNamespace("mpoly"))
+  p(mpoly, stars = TRUE, silent = TRUE, plus_pad = 0L, times_pad = 0L) |>
+    stringr::str_replace_all("[*]{2}", "^")
 }
