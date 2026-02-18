68f2395a37b742edf8df3d4c8ca4a3141b46b285
sonish13 <lamsal.sonish@gmail.com>
2026-02-18 12:41:37 -0600

    fix check files commit

--- check/vnorm.Rcheck/00_pkg_src/vnorm/R/reexport-mpoly.R
+++
@@ -1,3 +0,0 @@
-#' @importFrom mpoly mp
-#' @export
-mpoly::mp
--- check/vnorm.Rcheck/00_pkg_src/vnorm/R/vnorm-attach.R
+++
@@ -1,4 +0,0 @@
-.onLoad <- function(libname, pkgname) {
-  # Silence cmdstanr version-check startup chatter for package users.
-  options(cmdstanr_no_ver_check = TRUE)
-}
